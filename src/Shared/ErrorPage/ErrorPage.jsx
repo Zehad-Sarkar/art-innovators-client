c97ecf7501331920726a62e31232de3e238ee688
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-06-14 13:55:28 +0600

    error page added

--- src/Shared/ErrorPage/ErrorPage.jsx
+++ src/Shared/ErrorPage/ErrorPage.jsx
@@ -15,14 +15,14 @@ const ErrorPage = () => {
             Page not found
           </h1>
           <p className="mt-6 text-base leading-7 text-gray-600">
-            Sorry, we couldn’t find the page you’re looking for.
+            Sorry, We couldn’t find the page you’re looking for.
           </p>
           <div className="flex items-center justify-center mt-10 gap-x-6">
             <Link
               to="/"
               className="rounded-md bg-purple-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
             >
-              Go back home
+              Go back Home
             </Link>
           </div>
         </div>
