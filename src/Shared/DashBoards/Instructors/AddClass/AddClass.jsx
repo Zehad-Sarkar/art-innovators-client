a77941b4517a0994b67cfd944fa610dfc3b15601
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-06-14 13:44:45 +0600

    add a class page added

--- src/Shared/DashBoards/Instructors/AddClass/AddClass.jsx
+++ src/Shared/DashBoards/Instructors/AddClass/AddClass.jsx
@@ -150,7 +150,7 @@ const AddClass = () => {
             className="file-input file-input-bordered"
           />
         </div>
-
+        
         <input
           className=" hover:bg-slate-500"
           type="submit"
